3f58adf0d15116d4ddd20eca38eb3a36f51f71d2
jeyak <r.jeyaksan@outlook.com>
2016-12-13 23:29:46 +0100

    Correction des bugs

--- partie 3/Dictionnaire/gestrech.c
+++ partie 3/Dictionnaire/gestrech.c
@@ -24,14 +24,15 @@ unsigned int strDiff(char *str1Input, char *str2Input){
 
     unsigned int charDiff = 0;
     int i = 0;
-    int exit = 0;
+    int exit = 0; // Variable servant a sortir de la bloucle
     while(exit == 0){
         if(str1Input[i] != str2Input[i]){
-            charDiff++;
+            charDiff++; // On incremente a chaque fois que qu'on tombe sur un caractère different
         }
         i++;
         if(str1Input[i] == '\0' || str2Input[i] == '\0')
         {
+            // Ajout des caractère supplémentaire du plus string
             charDiff += (strlen(str1Input) > strlen(str2Input) ? strlen(str1Input) - strlen(str2Input) : strlen(str2Input) - strlen(str1Input));
             exit = 1;
         }
