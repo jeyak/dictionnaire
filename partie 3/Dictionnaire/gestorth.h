dd4cfd87b3928cca95f62652dd62b660bc0b1699
jeyak <r.jeyaksan@outlook.com>
2016-12-14 00:04:54 +0100

    P3 Gestorth

---
+++ partie 3/Dictionnaire/gestorth.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   gestorth.h
+ * Author: rjeya
+ *
+ * Created on 14 décembre 2016, 00:03
+ */
+
+#ifndef GESTORTH_H
+#define GESTORTH_H
+
+
+
+#endif /* GESTORTH_H */
+
