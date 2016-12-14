a93de80f33a343cd5f47411d7f8943fbd1159051
jeyak <r.jeyaksan@outlook.com>
2016-12-14 09:29:34 +0100

    P3 Gestorth

--- partie 3/Dictionnaire/gestorth.h
+++ partie 3/Dictionnaire/gestorth.h
@@ -14,7 +14,17 @@
 #ifndef GESTORTH_H
 #define GESTORTH_H
 
+typedef struct part3{
+    char wordTxt[50];
+    char wordDico[50];
+    FILE* fTxt;
+    FILE* fDico;
+    int resSearch;
+}part3;
 
+void afficheNotFound(int tp);
+void afficheAndWordProche(part3* p1, int tp);
+void correctTxt(part3* p2);
 
 #endif /* GESTORTH_H */
 
